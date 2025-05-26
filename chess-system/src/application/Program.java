ccd023f349c65031a125d332eae9330c146472c2
Nicolas-guilheme <nicolasrenovado12@gmail.com>
2025-05-26 18:45:44 -0300

    Testando

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		Board board = new Board(8, 8);
 		
 		
-		Position position = new Position(3, 5);
-		System.out.println(position);
+		System.out.println("TESTE");
+		System.out.println("TESTE");
+		System.out.println("TESTE");
+		System.out.println("TESTE");
+		System.out.println("TESTE");
 		
 	}
 	
