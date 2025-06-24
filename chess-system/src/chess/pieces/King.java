95660a84af3f78bf590a243a6b6fb26cdf2200c4
Nicolas-guilheme <nicolasrenovado12@gmail.com>
2025-06-24 14:15:55 -0300

    Testing somethings...

--- chess-system/src/chess/pieces/King.java
+++ chess-system/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,15 @@ public class King extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		
+		// above
+		p.setValues(position.getRow() - 1 , position.getColumn());
+		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { 
+			mat[p.getRow()][p.getColumn()] = true;	
+		}
+			
+		
 		return mat;
 	}
 
